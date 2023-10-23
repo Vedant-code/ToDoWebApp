189a3f592f1f46d5f3fbbd4da37916c29f31c64d
vedant-code <vedant.dhote20@pccoepune.org>
2023-10-23 14:00:45 +0530

    Added light/dark mode and UserAvatar

--- todof/components/Header.tsx
+++ todof/components/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Logo from "./Logo"
-
+import  DarkModeToggle  from './DarkmodeToggle'
+import UserButton from './UserButton'
 function Header() {
   return <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
-    <nav>
+    <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
         {/* {Logo} */}
         <Logo/>
+        <div className='flex-1 flex items-center justify-end space-x-4'>
+        {/* Darkmode */}
+            <DarkModeToggle/>
+        {/* UserButton */}
+            <UserButton/>
+        </div>
     </nav>
   </header>
 }
